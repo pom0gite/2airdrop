d1e5675541e3f9ca1cf71977ba490698056cda90
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-12 00:48:07 +0300

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Simple ERC20 token for Base mini project
+
+contract Token {
+    string public name = "BaseToken";
+    string public symbol = "BTK";
+    uint public totalSupply = 1000000;
+
+    // TODO: implement balances
+    // TODO: add transfer function
+
+// add mint function
+// restrict mint to owner
+// add burn logic
+// improve token economics
+// fix supply logic
+}
