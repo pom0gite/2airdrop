9ba4a749fcdd8e175d6efcc0ad387e13ab76f5f5
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-12 13:29:18 +0300

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Simple NFT contract for Base mini project
+
+contract NFT {
+    string public name = "BaseNFT";
+    string public symbol = "BNFT";
+
+    // TODO: implement mint NFT
+    // TODO: add token URI
+}
+// add metadata support
+// improve mint logic
+// restrict access
+// add collection description
