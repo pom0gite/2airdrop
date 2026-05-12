6df517145838dd311ae1502a5f4089d29b9a9d00
pom0gite <98287395+pom0gite@users.noreply.github.com>
2026-05-12 22:43:40 +0300

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// Voting contract for Base project
+
+contract Voting {
+
+    // TODO: create proposal
+    // TODO: vote function
+    // TODO: count votes
+
+}
+// prevent double voting
+// add deadline
+// improve voting logic
+// add result calculation
